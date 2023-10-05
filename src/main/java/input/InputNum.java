da757d4c5acf486c396028daeb47156713479c7d
qqri <cqo1154@gmail.com>
2023-10-05 21:11:45 +0900

    feat : input 숫자 예외 설정

---
+++ src/main/java/input/InputNum.java
@@ -0,0 +1,18 @@
+package input;
+import java.util.regex.Matcher;
+public class InputNum {
+    int input;
+
+    public InputNum(String text) {
+        this.input = makeInput(text);
+    }
+
+    public int makeInput(String text) {
+        check(text);
+        return Integer.parseInt(text);
+    }
+
+    private void check(String text) {
+        if(text.matches("[^0-9]")) throw new NumberFormatException("회수에는 숫자만 입력 가능합니다.");
+    }
+}
